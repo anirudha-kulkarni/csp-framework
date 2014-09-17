8d602b732ccf55f645ee46a6fa10d83ae8227d34
Smit Jawale <sjawale@genius-minds.com>
2014-09-17 14:15:01 +0530

    1. Added functionality for setting Authentication Timeout for Internal and External IP 2. Changed People to Users 3. Changed Clients to Client Profile 4. Made website linkable in Client List 5. Changed name of Customer-Vendor to Vendor Management

--- CSPFramework_Dev/framework.cspnetworks.net/Global.asax.cs
+++ CSPFramework_Dev/framework.cspnetworks.net/Global.asax.cs
@@ -8,6 +8,8 @@ using System.Web.Routing;
 using System.Web.Http;
 using framework.cspnetworks.net.CustomBinders;
 using CSPLibrary;
+using System.Web.SessionState;
+using System.Web.Security;
 
 
 namespace framework.cspnetworks.net
@@ -26,5 +28,30 @@ namespace framework.cspnetworks.net
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            //Only access session state if it is available
+            if (Context.Handler is IRequiresSessionState || Context.Handler is IReadOnlySessionState)
+            {
+                //If we are authenticated AND we dont have a session here.. redirect to login page.
+                HttpCookie authenticationCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authenticationCookie != null)
+                {
+                    FormsAuthenticationTicket authenticationTicket = FormsAuthentication.Decrypt(authenticationCookie.Value);
+                    if (!authenticationTicket.Expired)
+                    {
+                        //of course.. replace ANYKNOWNVALUEHERETOCHECK with "UserId" or something you set on the login that you can check here to see if its empty.
+                        // if (Session["ASP.NET_SessionId"] == null)
+                        if (Session.Contents.Count == 0)
+                        {
+                            //This means for some reason the session expired before the authentication ticket. Force a login.
+                            FormsAuthentication.SignOut();
+                            Response.Redirect(FormsAuthentication.LoginUrl, true);
+                            return;
+                        }
+                    }
+                }
+            }
+        } 
     }
 }
---
+++ CSPFramework_Dev/framework.cspnetworks.net/Helpers/ConfigHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace framework.cspnetworks.net.Helpers
+{
+    public class ConfigHelper
+    {
+        public static string InternalIPAddress
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["Internal_IP"].ToString();
+            }
+        }
+        public static int InternalExpiration
+        {
+            get
+            {
+                return Convert.ToInt32(ConfigurationManager.AppSettings["Internal_Expiration"]);
+            }
+        }
+        public static int ExternalExpiration
+        {
+            get
+            {
+                return Convert.ToInt32(ConfigurationManager.AppSettings["External_Expiration"]);
+            }
+        }
+        public static int InternalSessionExpiration
+        {
+            get
+            {
+                return Convert.ToInt32(ConfigurationManager.AppSettings["Internal_Session_Timeout"]);
+            }
+        }
+        public static int ExternalSessionExpiration
+        {
+            get
+            {
+                return Convert.ToInt32(ConfigurationManager.AppSettings["External_Session_Timeout"]);
+            }
+        }
+    }
+}
